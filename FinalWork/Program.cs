0efe333b5bb454d50acfbc8a91ff0672790b92dc
Irina Tsarevski <irinatsarevski@icloud.com>
2023-01-27 13:13:45 +0100

    wroted the programm

---
+++ FinalWork/Program.cs
@@ -0,0 +1,44 @@
+﻿string[]NewArrayString (string[]arrString)
+{
+    int lengthArray = arrString.Length;
+    int lengthElement;
+    int count = 0;
+   
+    for(int i = 0; i < lengthArray; i++)
+    {
+        lengthElement = arrString[i].Length;
+        if(lengthElement < 4) count++;
+    }
+
+    string [] result = new string [count];
+    int j = 0;
+
+    for(int i = 0; i < lengthArray; i++)
+    {   
+        lengthElement = arrString[i].Length;
+        if(lengthElement < 4)
+        {
+            result[j] = arrString[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string [] array)
+{
+    //int count = array.Length;
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+string [] arrString = {"text", "oh", "cat", "mouse", "ex", "mom5", "wow", "cool"};
+PrintArray (arrString);
+Console.WriteLine();
+string [] newArrString = NewArrayString(arrString);
+PrintArray (newArrString);
+Console.WriteLine();
